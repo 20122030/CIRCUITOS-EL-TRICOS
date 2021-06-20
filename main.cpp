d278f94492fee6036c36a62ae301181dea683b15
Miqueias Araujo <82775753+20122030@users.noreply.github.com>
2021-06-20 21:01:20 +0000

    main.cpp

--- main.cpp
+++ main.cpp
@@ -1,50 +1,15 @@
 #include <iostream>
+#include "circuitoseletricos.h"
 using namespace std;
-#ifndef circuitoseletricos_H
-#define circuitoseletricos_H
 
-struct tCircuito
+int main ()
 {
-  int valorR1, valorR2, valorfonteV1;
-}
+  tcircuito circuitoseletricos;
 
-void calcserie(float R1, float R2)
-{
-float Resistserie = 0.0;
-Resistserie = R1+R2;
+  cout << "digite o valor de R1" << endl;
+  cin >> circuitoseletricos.valorR1;
 
-cout << "O valor da Resistência em serie =" << Resistserie << endl;
-}
-
-void calparalelo (float R1, float R2)
-{
-  float Resistparalelo = 0.0;
-  Resistparalelo = (R1*R2/R1+R2);
-  
-  cout << "O valor da Resistência em serie =" << Resistparalelo << endl;
-}
-
-void correntetotal (float R1, float R2, float fonteV1)
-{
-  float Itotal = 0.0;
-  Itotal = (fonteV1)/(R1=R2);
-
-  cout << "A corrente total do circuito =" << Itotal << endl;
-}
-
-
-void calcpotencia (float R1, float R2, float fonteV1)
-{
-  float potfonte = 0.0;
-  float Requivalente = 0.0;
-  Requivalente = R1+R2;
-  potfonte = (pow(fonteV1,2))/(Requivalente);
-
-  cout << " A Potencia da fonte = " << potfonte << endl;
-  
-}
-#endif
-
-int main() {
+  cout << "digite o valor de R2" << endl;
+  cin >> circuitoseletricos.valorR2;
 
 }
