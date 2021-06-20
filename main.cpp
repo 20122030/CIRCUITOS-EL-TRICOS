bd8a0b4ab448febeca53cef689c4931541057f24
Miqueias Araujo <82775753+20122030@users.noreply.github.com>
2021-06-20 19:00:10 +0000

    função resistência em paralelo

--- main.cpp
+++ main.cpp
@@ -8,13 +8,21 @@ struct tCircuito
   int valorR1, valorR2, valorfonteV1;
 }
 
-void calcserie()
-float R1, R2, Resistserie;
-Resitserie = R1 + R2;
+void calcserie(float R1, float R2)
+{
+float Resistserie = 0.0;
+Resistserie = R1+R2;
 
 cout << "O valor da Resistência em serie =" << Resistserie << endl;
+}
 
-
+void calparalelo (float R1, float R2)
+{
+  float Resistparalelo = 0.0;
+  Resistparalelo = (R1*R2/R1+R2);
+  
+  cout << "O valor da Resistência em serie =" << Resistparalelo << endl;
+}
 
 int main() {
 
